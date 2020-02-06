ce1d831fdb9c9e7952fa88516619b06d476f7533
MrBayo <el.bayoan@gmail.com>
2020-02-06 01:56:27 -0400

    FirstCommit

---
+++ WebTest/AddigyAutomation/src/main/java/com/addigy/automation/AddigyAutomation/App.java
@@ -0,0 +1,13 @@
+package com.addigy.automation.AddigyAutomation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
